d1443452397d7f10e2524e7e22c29da1153a36e2
fkhrmln <fkhrmhsn110802@gmail.com>
2022-11-22 21:40:58 +0700

    remove firebase instance crash

--- core/test/helpers/test_helper.dart
+++ core/test/helpers/test_helper.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:core/data/datasources/db/database_helper.dart';
 import 'package:core/data/datasources/db/database_series_helper.dart';
 import 'package:core/data/datasources/movie_local_data_source.dart';
@@ -8,8 +6,6 @@ import 'package:core/data/datasources/series_local_data_source.dart';
 import 'package:core/data/datasources/series_remote_data_source.dart';
 import 'package:core/domain/repositories/movie_repository.dart';
 import 'package:core/domain/repositories/series_repository.dart';
-import 'package:flutter/services.dart';
-import 'package:http/io_client.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 
